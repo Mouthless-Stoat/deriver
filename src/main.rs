a221c7f43ab8bda9d935682da872d6722735b189
Mouthless Stoat <nphuyanh@outlook.com>
2025-04-23 09:54:45 -0300

    feat: add ln as a function and optimize more exponent rule

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,10 @@ impl Expr {
         Self::Log(Box::new(self), Box::new(f.into()))
     }
 
+    fn ln(f: impl Into<Expr>) -> Self {
+        Self::Log(Box::new(E.into()), Box::new(f.into()))
+    }
+
     /// Apply a trig function to this value.
     fn trig(self, func: Trig) -> Self {
         Self::Trig(func, Box::new(self))
@@ -95,23 +99,38 @@ impl Expr {
                 .mul(*g.clone())
                 .add(f.mul(g.clone().derive()))
                 .div(g.exp(2.0)),
+
             Self::Mul(f, g) => f.clone().derive().mul(*g.clone()).add(g.derive().mul(*f)),
 
             Self::Exp(f, g) => match (*f, *g) {
+                // Variable convention:
+                // x for any variable
+                // f for any function
+                // a for any exponent
+                // n for the value of any exponent
+                // f for any function
+
                 // x^n -> nx^(n-1)
                 (x @ Self::Var, a @ Self::Num(n)) => a.mul(x.exp(n - 1.0)),
 
                 // f(x)^n
                 (f, a @ Self::Num(n)) => a.mul(f.clone().exp(n - 1.0)).mul(f.derive()),
 
+                // e^x
+                (e @ Self::Num(E), x @ Self::Var) => e.exp(x),
+
+                // e^f(x)
+                (e @ Self::Num(E), f) => e.exp(f.clone()).mul(f.derive()),
+
                 // n^x
-                (a @ Self::Num(_), x @ Self::Var) => Self::Num(1.0).div(x.mul(Self::Num(E).log(a))),
+                (a @ Self::Num(_), x @ Self::Var) => a.clone().exp(x).mul(Self::ln(a)),
 
                 // n^f(x)
-                (a @ Self::Num(_), f) => a.clone().exp(f.clone()).mul(Self::Num(E).log(a)).mul(f.derive()),
+                (a @ Self::Num(_), f) => a.clone().exp(f.clone()).mul(Self::ln(a)).mul(f.derive()),
 
                 _ => panic!("Exponents not in the form of f(x)^n or n^f(x) where n is a number are too complicated can't compute"),
             },
+
             Self::Log(f, g) => match (*f, *g) {
                 (a @ Self::Num(_), f) => f.clone().derive().div(f.mul(Expr::Num(E).log(a))),
                 _ => panic!("Logs not in the form of log_n (f(x)) where n is a number are too complicated can't compute"),
